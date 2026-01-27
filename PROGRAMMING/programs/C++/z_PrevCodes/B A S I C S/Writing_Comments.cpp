ec421a7c2c339abf0c6ff6d80c87a6864b69f624
Ansh Kukreja <anshkukrejapc@gmail.com>
2026-01-27 11:30:30 +0530

    OOPS

--- PROGRAMMING/programs/C++/z_PrevCodes/B A S I C S/Writing_Comments.cpp
+++
@@ -1,7 +0,0 @@
-//This is a single line comment.
-
-/*This is a multi line comment
-as you can see, i am using
-multiple
-lines 
-here*/
--- PROGRAMMING/programs/C++/z_PrevCodes/P R A C T I S E/Find_The_Single_no_.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main(){
-    int c;
-
-    c=2^8^9^5^4^2^8^9^5;
-    cout<<c;
-}
